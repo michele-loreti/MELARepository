b6a5d50b3509100f393c6851467ea4b32e26585a
Michele Loreti <michele.loreti@gmail.com>
2018-05-22 15:03:47 +0200

    Update Rule.java

--- src/Model/Rule.java
+++ src/Model/Rule.java
@@ -3,7 +3,7 @@ package Model;
 @FunctionalInterface
 public interface Rule {
 	
-	public ActionStep apply( int locationIndex, int agentIndex);
+	public ActionStep apply(AgentManager am, LocationManager lm, int locationIndex, int agentIndex);
 	
 
 }
